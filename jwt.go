d1d8fabba49e7538b93b8e7a25c0b39b34cb6ca7
Vitaly Berg <bergusman@gmail.com>
2021-08-15 19:57:05 +0800

    Test and document JWT

--- jwt.go
+++ jwt.go
@@ -1,24 +1,38 @@
 package apns
 
 import (
-	"crypto"
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"math/big"
 )
 
-func i2osp(n *big.Int, l int) []byte {
-	r := make([]byte, l)
-	b := n.Bytes()
-	copy(r[l-len(b):], b)
-	return r
+// See "Create and Encrypt Your JSON Token" section in
+// https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/establishing_a_token-based_connection_to_apns.
+
+// Private key is not ECDSA key with P-256 curve.
+var ErrJWTKeyNotECDSAP256 = errors.New("jwt: not ECDSA P-256 key")
+
+// i2osp is an I2OSP (Integer to Octet Stream Primitive) function.
+func i2osp(n *big.Int) []byte {
+	return n.FillBytes(make([]byte, 32))
 }
 
-func es256(key *ecdsa.PrivateKey, body string) ([]byte, error) {
-	h := crypto.SHA256.New()
-	_, err := h.Write([]byte(body))
+// es256 signs an input (JWS Signing Input)
+// using Curve Digital Signature Algorithm (ECDSA)
+// by key with P-256 curve type, specified IN RFC 7515.
+// Returns digital signature (JWS Signature).
+func es256(key *ecdsa.PrivateKey, input []byte) ([]byte, error) {
+	if key.Curve != elliptic.P256() {
+		return nil, ErrJWTKeyNotECDSAP256
+	}
+
+	h := sha256.New()
+	_, err := h.Write(input)
 	if err != nil {
 		return nil, err
 	}
@@ -28,17 +42,22 @@ func es256(key *ecdsa.PrivateKey, body string) ([]byte, error) {
 		return nil, err
 	}
 
-	return append(i2osp(r, 32), i2osp(s, 32)...), nil
+	return append(i2osp(r), i2osp(s)...), nil
 }
 
+// GenerateBearer creates JSON token with keyID, teamID and issuedAt and
+// encrypts by an authentication token signing key with the ES256 algorithm.
+// Returns encrypted token that used in the authorization header of a notification request
+// as bearer <token data>.
 func GenerateBearer(key *ecdsa.PrivateKey, keyID, teamID string, issuedAt int64) (string, error) {
-	h := fmt.Sprintf(`{"alg":"ES256","typ":"JWT","kid":"%s"}`, keyID)
-	p := fmt.Sprintf(`{"iss":"%s","iat":%d}`, teamID, issuedAt)
-	hp := base64.RawURLEncoding.EncodeToString([]byte(h)) + "." + base64.RawURLEncoding.EncodeToString([]byte(p))
-	sig, err := es256(key, hp)
+	// See RFC 7519 for JWT and RFC 7515 for JWS.
+	header := fmt.Sprintf(`{"alg":"ES256","typ":"JWT","kid":"%s"}`, keyID) // JOSE Header (JWT Protected Header)
+	payload := fmt.Sprintf(`{"iss":"%s","iat":%d}`, teamID, issuedAt)      // JWT Claims (JWS Payload)
+	unsecured := base64.RawURLEncoding.EncodeToString([]byte(header)) + "." + base64.RawURLEncoding.EncodeToString([]byte(payload))
+	sig, err := es256(key, []byte(unsecured)) // JWS Signature
 	if err != nil {
 		return "", err
 	}
-	t := hp + "." + base64.RawURLEncoding.EncodeToString([]byte(sig))
+	t := unsecured + "." + base64.RawURLEncoding.EncodeToString([]byte(sig)) // JWS Compact Serialization
 	return t, nil
 }
--- jwt_test.go
+++ jwt_test.go
@@ -1,26 +1,164 @@
 package apns
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"math/big"
+	"strings"
 	"testing"
 	"time"
 )
 
-func ExampleGenerateBearer() {
-
-}
-
 func TestI2OSP(t *testing.T) {
 	n := big.NewInt(1337)
-	h := hex.EncodeToString(i2osp(n, 32))
+	h := hex.EncodeToString(i2osp(n))
 	if h != "0000000000000000000000000000000000000000000000000000000000000539" {
-		t.Errorf("%q", h)
+		t.Errorf("got: %q", h)
+	}
+}
+
+func TestES256KeyNotP256(t *testing.T) {
+	curves := []elliptic.Curve{elliptic.P224(), elliptic.P384(), elliptic.P521()}
+	for _, curve := range curves {
+		key, err := ecdsa.GenerateKey(curve, rand.Reader)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = es256(key, []byte("input"))
+		if err != nil {
+			if err != ErrJWTKeyNotECDSAP256 {
+				t.Errorf("curve %v: want: ErrJWTKeyNot256Bits got: %q", curve.Params().Name, err.Error())
+			}
+		} else {
+			t.Errorf("curve %v: want not nil err", curve.Params().Name)
+		}
+	}
+}
+
+func TestES256Key(t *testing.T) {
+	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	input := []byte("input")
+	sig, err := es256(key, input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r, s := new(big.Int), new(big.Int)
+	r.SetBytes(sig[:32])
+	s.SetBytes(sig[32:])
+
+	h := sha256.New()
+	_, err = h.Write(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !ecdsa.Verify(&key.PublicKey, h.Sum(nil), r, s) {
+		t.Error("cannot verify ES256 signature")
+	}
+}
+
+func TestGenerateBearer(t *testing.T) {
+	key, err := AuthKeyFromFile("testdata/AuthKey_5MDQ4KLTY7.p8")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	keyID := "5MDQ4KLTY7"
+	teamID := "SUPERTEEM1"
+	issuedAt := time.Now().Unix()
+
+	token, err := GenerateBearer(key, keyID, teamID, issuedAt)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		t.Error("token must have three parts spearated by dot")
+	}
+
+	unsecured := []byte(parts[0] + "." + parts[1])
+
+	sig, err := base64.RawURLEncoding.DecodeString(parts[2])
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sig) != 64 {
+		t.Fatalf("len(sig): %v want: 64", len(sig))
+	}
+
+	r, s := new(big.Int), new(big.Int)
+	r.SetBytes(sig[:32])
+	s.SetBytes(sig[32:])
+
+	h := sha256.New()
+	_, err = h.Write(unsecured)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !ecdsa.Verify(&key.PublicKey, h.Sum(nil), r, s) {
+		t.Error("cannot verify ES256 signature")
+	}
+
+	// Header
+	header64, err := base64.RawStdEncoding.DecodeString(parts[0])
+	if err != nil {
+		t.Fatalf("cannot decode header from base64: %v", err)
+	}
+
+	var header struct {
+		Alg string
+		Typ string
+		Kid string
+	}
+	err = json.Unmarshal(header64, &header)
+	if err != nil {
+		t.Fatalf("cannot decode header from json: %v", err)
+	}
+
+	if header.Alg != "ES256" {
+		t.Errorf("header.alg: %v want: %v", header.Alg, teamID)
+	}
+	if header.Kid != keyID {
+		t.Errorf("header.kid: %v want: %v", header.Kid, issuedAt)
+	}
+
+	// Payload
+	payload64, err := base64.RawStdEncoding.DecodeString(parts[1])
+	if err != nil {
+		t.Fatalf("cannot decode payload from base64: %v", err)
+	}
+
+	var payload struct {
+		Iss string
+		Iat int64
+	}
+	err = json.Unmarshal(payload64, &payload)
+	if err != nil {
+		t.Fatalf("cannot decode payload from json: %v", err)
+	}
+
+	if payload.Iss != teamID {
+		t.Errorf("payload.iss: %v want: %v", payload.Iss, teamID)
+	}
+	if payload.Iat != issuedAt {
+		t.Errorf("payload.iat: %v want: %v", payload.Iat, issuedAt)
 	}
 }
 
 func BenchmarkGenerateBearer(b *testing.B) {
-	key, err := AuthKeyFromFile("testdata/AuthKeyValid.p8")
+	key, err := AuthKeyFromFile("testdata/AuthKey_5MDQ4KLTY7.p8")
 	if err != nil {
 		b.Fatal(err)
 	}
