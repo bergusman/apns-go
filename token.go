4cbb9665d1429b69f96481d0566f27161ac2de18
Vitaly Berg <bergusman@gmail.com>
2021-08-17 01:02:55 +0800

    Document and test token

--- token.go
+++ token.go
@@ -10,19 +10,47 @@ import (
 
 // See https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/establishing_a_token-based_connection_to_apns.
 
+var ErrTokenKeyNil = errors.New("token: key is nil")
+
+// For security, APNs requires you to refresh your token regularly.
+// Refresh your token no more than once every 20 minutes
+// and no less than once every 60 minutes.
+// APNs rejects any request whose token
+// contains a timestamp that is more than one hour old.
+// Similarly, APNs reports an error if you recreate your tokens
+// more than once every 20 minutes.
+const TokenRefreshInterval = 2400 // 40 minutes
+
 // Token represents JSON Token used for token-based connection to APNs.
 type Token struct {
 	sync.Mutex
 
-	// Authentication token signing key
-	Key             *ecdsa.PrivateKey
-	KeyID           string
-	TeamID          string
-	RefreshInterval int64
-	IssuedAt        int64
-	Bearer          string
+	// Authentication token signing key extracted from a text file (with a .p8 file extension).
+	// Use AuthKeyFromFile or AuthKeyFromBytes.
+	Key *ecdsa.PrivateKey
+
+	// The 10-character Key ID you obtained from your developer account
+	// with an authentication token signing key.
+	KeyID string
+
+	// The value is the 10-character Team ID you use for developing your company’s apps.
+	// Obtain this value from your developer account.
+	TeamID string
+
+	// Time window in from IssuedAt to IssuedAt + RefreshInterval
+	// when Bearer is not expired.
+	RefreshInterval int64 // in seconds
+
+	// Time at Bearer was generated.
+	IssuedAt int64 // Epoch time in seconds
+
+	// Generated JWT Token for APNs request authorization at IssuedAt time.
+	Bearer string
 }
 
+// NewToken returns Token with key, keyID, teamID with default TokenRefreshInterval.
+// How obtain credentials,
+// see https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/establishing_a_token-based_connection_to_apns#2943371
 func NewToken(key *ecdsa.PrivateKey, keyID, teamID string) *Token {
 	return &Token{
 		Key:    key,
@@ -35,7 +63,7 @@ func (t *Token) Expired() bool {
 	if t.RefreshInterval > 0 {
 		return time.Now().Unix() > t.IssuedAt+t.RefreshInterval
 	} else {
-		return time.Now().Unix() > t.IssuedAt+2400 // 40 minutes
+		return time.Now().Unix() > t.IssuedAt+TokenRefreshInterval
 	}
 }
 
@@ -50,7 +78,7 @@ func (t *Token) GenerateIfExpired() (string, error) {
 
 func (t *Token) Generate() (string, error) {
 	if t.Key == nil {
-		return "", errors.New("key is nil")
+		return "", ErrTokenKeyNil
 	}
 	issuedAt := time.Now().Unix()
 	bearer, err := GenerateBearer(t.Key, t.KeyID, t.TeamID, issuedAt)
--- token_test.go
+++ token_test.go
@@ -1,1 +1,141 @@
 package apns
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestTokenKeyNil(t *testing.T) {
+	token := NewToken(nil, "5JZB9P77A7", "SUPERTEEM1")
+	_, err := token.Generate()
+	if err != nil {
+		if err != ErrTokenKeyNil {
+			t.Errorf("err: %v; want: ErrTokenKeyNil", err)
+		}
+	} else {
+		t.Error("err must be not nil")
+	}
+}
+
+func TestTokenKeyInvalid(t *testing.T) {
+	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := NewToken(key, "5JZB9P77A7", "SUPERTEEM1")
+	_, err = token.Generate()
+	if err != nil {
+		if err != ErrJWTKeyNotECDSAP256 {
+			t.Errorf("err: %v; want: ErrJWTKeyNotECDSAP256", err)
+		}
+	} else {
+		t.Error("err must be not nil")
+	}
+}
+
+func TestTokenExpired(t *testing.T) {
+	token := &Token{}
+	if !token.Expired() {
+		t.Error("token must be expired")
+	}
+
+	token.IssuedAt = time.Now().Add(-(TokenRefreshInterval + 1) * time.Second).Unix()
+	if !token.Expired() {
+		t.Error("token must be expired")
+	}
+
+	token.RefreshInterval = 60
+	token.IssuedAt = time.Now().Add(-61 * time.Second).Unix()
+	if !token.Expired() {
+		t.Error("token must be expired")
+	}
+}
+
+func TestTokenNotExpired(t *testing.T) {
+	token := &Token{
+		IssuedAt: time.Now().Unix(),
+	}
+	if token.Expired() {
+		t.Error("token must be not expired")
+	}
+
+	token.IssuedAt = time.Now().Add(-(TokenRefreshInterval - 1) * time.Second).Unix()
+	if token.Expired() {
+		t.Error("token must be not expired")
+	}
+
+	token.RefreshInterval = 3600
+	token.IssuedAt = time.Now().Add(-3599 * time.Second).Unix()
+	if token.Expired() {
+		t.Error("token must be not expired")
+	}
+}
+
+func TestTokenGenerateIfExpired(t *testing.T) {
+	key, err := AuthKeyFromFile("testdata/AuthKey_5MDQ4KLTY7.p8")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := NewToken(key, "5JZB9P77A7", "SUPERTEEM1")
+
+	want, err := token.GenerateIfExpired()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := token.GenerateIfExpired()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != want {
+		t.Errorf("got: %v; want: %v", got, want)
+	}
+}
+
+func TestSetAuthorization(t *testing.T) {
+	key, err := AuthKeyFromFile("testdata/AuthKey_5MDQ4KLTY7.p8")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := NewToken(key, "5JZB9P77A7", "SUPERTEEM1")
+	h := make(http.Header)
+	token.SetAuthorization(h)
+
+	if h.Get("authorization") != "bearer "+token.Bearer {
+		t.Errorf("invalid authorization header: %v", h.Get("authorization"))
+	}
+}
+
+func TestSetAuthorizationWithInvalidKey(t *testing.T) {
+	key, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := NewToken(key, "5JZB9P77A7", "SUPERTEEM1")
+	h := make(http.Header)
+	err = token.SetAuthorization(h)
+	if err != nil {
+		if err != ErrJWTKeyNotECDSAP256 {
+			t.Errorf("err: %v; want: ErrJWTKeyNotECDSAP256", err)
+		}
+	} else {
+		t.Error("err must be not nil")
+	}
+}
+
+func TestSetBearer(t *testing.T) {
+	bearer := "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjVKWkI5UDc3QTcifQ.eyJpc3MiOiJTVVBFUlRFRU0xIiwiaWF0IjoxNjI5MDAwMDAwfQ.f9paE35q8NzQyrnJaDbYb1yBzBcj8_jHpeMe06y5OERf-0ERD70VZCS6dG-toWKSxeevYEARKMwoGliJtZ7atQ"
+	h := make(http.Header)
+	SetBearer(h, bearer)
+	if h.Get("authorization") != "bearer "+bearer {
+		t.Errorf("invalid authorization header: %v", h.Get("authorization"))
+	}
+}
