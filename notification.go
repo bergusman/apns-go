9f622c1a7077826e2aa0f0d8e06a857a82ccb242
Vitaly Berg <bergusman@gmail.com>
2021-08-17 01:03:14 +0800

    Document and test notification

--- notification.go
+++ notification.go
@@ -26,6 +26,7 @@ const (
 	HostProductionPort2197 = "https://api.push.apple.com:2197"
 )
 
+// Values for Notification.PushType field.
 const (
 	// Use the alert push type for notifications that trigger
 	// a user interaction—for example, an alert, badge, or sound.
@@ -50,31 +51,135 @@ const (
 	// It is recommended on macOS, iOS, tvOS, and iPadOS.
 	PushTypeBackground = "background"
 
-	PushTypeVoIP         = "voip"
+	// Use the voip push type for notifications that provide information about
+	// an incoming Voice-over-IP (VoIP) call. For more information,
+	// see https://developer.apple.com/documentation/pushkit/responding_to_voip_notifications_from_pushkit.
+	//
+	// If you set this push type, the apns-topic header field must use your app’s bundle ID
+	// with .voip appended to the end. If you’re using certificate-based authentication,
+	// you must also register the certificate for VoIP services.
+	// The topic is then part of the 1.2.840.113635.100.6.3.4 or 1.2.840.113635.100.6.3.6 extension.
+	//
+	// The voip push type is not available on watchOS.
+	// It is recommended on macOS, iOS, tvOS, and iPadOS.
+	PushTypeVoIP = "voip"
+
+	// Use the complication push type for notifications
+	// that contain update information for a watchOS app’s complications.
+	// For more information,
+	// see https://developer.apple.com/documentation/clockkit/keeping_your_complications_up_to_date.
+	//
+	// If you set this push type, the apns-topic header field
+	// must use your app’s bundle ID with .complication appended to the end.
+	// If you’re using certificate-based authentication,
+	// you must also register the certificate for WatchKit services.
+	// The topic is then part of the 1.2.840.113635.100.6.3.6 extension.
+	//
+	// The complication push type is recommended for watchOS and iOS.
+	// It is not available on macOS, tvOS, and iPadOS.
 	PushTypeComplication = "complication"
+
+	// Use the fileprovider push type to signal changes to a File Provider extension.
+	// If you set this push type, the apns-topic header field
+	// must use your app’s bundle ID with .pushkit.fileprovider appended to the end.
+	// For more information,
+	// see https://developer.apple.com/documentation/fileprovider/content_and_change_tracking/tracking_your_file_provider_s_changes/using_push_notifications_to_signal_changes.
+	//
+	// The fileprovider push type is not available on watchOS.
+	// It is recommended on macOS, iOS, tvOS, and iPadOS.
 	PushTypeFileprovider = "fileprovider"
-	PushTypeMDM          = "mdm"
+
+	// Use the mdm push type for notifications
+	// that tell managed devices to contact the MDM server.
+	// If you set this push type, you must use the topic
+	// from the UID attribute in the subject
+	// of your MDM push certificate.
+	// For more information,
+	// see https://developer.apple.com/documentation/devicemanagement.
+	//
+	// The mdm push type is not available on watchOS.
+	// It is recommended on macOS, iOS, tvOS, and iPadOS.
+	PushTypeMDM = "mdm"
 )
 
+// Values for Notification.Priority field.
 const (
-	PriorityLow  = 5
+	// Specify 10 to send the notification immediately.
 	PriorityHigh = 10
+
+	// Specify 5 to send the notification based on power considerations on the user’s device.
+	PriorityLow = 5
 )
 
 type Notification struct {
+	// The device token is the hexadecimal bytes that identify the user’s device.
+	// Your app receives the bytes for this device token w
+	// hen registering for remote notifications;
+	// see https://developer.apple.com/documentation/usernotifications/registering_your_app_with_apns.
 	DeviceToken string
-	Host        string
 
-	ID         string // header: apns-id
-	Topic      string // header: apns-topic
-	PushType   string // header: apns-push-type
+	Host string
+
+	// A canonical UUID that is the unique ID for the notification.
+	// If an error occurs when sending the notification,
+	// APNs includes this value when reporting the error to your server.
+	// If you omit this header,
+	// APNs creates a UUID for you and returns it in its response.
+	ID string // header: apns-id
+
+	// The topic for the notification.
+	// In general, the topic is your app’s bundle ID/app ID.
+	// It can have a suffix based on the type of push notification.
+	// If you are using a certificate that supports Pushkit VoIP
+	// or watchOS complication notifications,
+	// you must include this header with bundle ID of you app
+	// and if applicable, the proper suffix.
+	// If you are using token-based authentication with APNs,
+	// you must include this header with the correct bundle ID
+	// and suffix combination.
+	Topic string // header: apns-topic
+
+	// The value of this header must accurately reflect the contents
+	// of your notification’s payload. If there is a mismatch,
+	// or if the header is missing on required systems,
+	// APNs may return an error, delay the delivery of the notification,
+	// or drop it altogether.
+	// Required for watchOS 6 and later; recommended for macOS, iOS, tvOS, and iPadOS.
+	PushType string // header: apns-push-type
+
+	// The date at which the notification is no longer valid.
+	// This value is a UNIX epoch expressed in seconds (UTC).
+	// If the value is nonzero, APNs stores the notification
+	// and tries to deliver it at least once,
+	// repeating the attempt as needed until the specified date.
+	// If the value is 0, APNs attempts to deliver
+	// the notification only once and doesn’t store it.
 	Expiration string // header: apns-expiration
-	Priority   int    // header: apns-priority
+
+	// The priority of the notification.
+	// If you omit this header, APNs sets the notification priority to 10.
+	// Specify 10 to send the notification immediately.
+	// Specify 5 to send the notification based on power considerations on the user’s device.
+	Priority int // header: apns-priority
+
+	// An identifier you use to coalesce multiple notifications
+	// into a single notification for the user.
+	// Typically, each notification request causes
+	// a new notification to be displayed on the user’s device.
+	// When sending the same notification more than once,
+	// use the same value in this header to coalesce the requests.
+	// The value of this key must not exceed 64 bytes.
 	CollapseID string // header: apns-collapse-id
 
+	// The JSON payload with the notification’s content.
+	// The JSON payload must not be compressed
+	// and is limited to a maximum size of 4 KB (4096 bytes).
+	// For a Voice over Internet Protocol (VoIP) notification,
+	// the maximum size is 5 KB (5120 bytes).
 	Payload interface{}
 }
 
+// MarshalJSON marshals the notification payload to JSON.
 func (n *Notification) MarshalJSON() ([]byte, error) {
 	switch v := n.Payload.(type) {
 	case string:
@@ -86,10 +191,14 @@ func (n *Notification) MarshalJSON() ([]byte, error) {
 	}
 }
 
+// The path to the device token.
+// The value of this header is /3/device/<device_token>.
 func (n *Notification) Path() string {
 	return "/3/device/" + n.DeviceToken
 }
 
+// URL builds full URL of remote notification request.
+// If Host field omitted HostProduction will be used.
 func (n *Notification) URL() string {
 	host := n.Host
 	if host == "" {
@@ -103,6 +212,7 @@ func (n *Notification) BuildRequest() (*http.Request, error) {
 	return n.BuildRequestWithContext(context.Background())
 }
 
+// BuildRequestWithContext builds request with context.
 func (n *Notification) BuildRequestWithContext(ctx context.Context) (*http.Request, error) {
 	payload := new(bytes.Buffer)
 	err := json.NewEncoder(payload).Encode(n)
--- notification_test.go
+++ notification_test.go
@@ -2,8 +2,136 @@ package apns
 
 import (
 	"encoding/json"
+	"net/http"
 	"testing"
 )
+
+func TestNotificationPayload(t *testing.T) {
+	tests := []struct {
+		payload interface{}
+		want    []byte
+	}{
+		{
+			payload: `{"aps":{"alert":{"title":"Hello"}}}`,
+			want:    []byte(`{"aps":{"alert":{"title":"Hello"}}}`),
+		},
+		{
+			payload: []byte(`{"aps":{"alert":{"title":"Hello"}}}`),
+			want:    []byte(`{"aps":{"alert":{"title":"Hello"}}}`),
+		},
+		{
+			payload: map[string]interface{}{
+				"aps": map[string]interface{}{
+					"alert": map[string]interface{}{
+						"title": "Hello",
+					},
+				},
+			},
+			want: []byte(`{"aps":{"alert":{"title":"Hello"}}}`),
+		},
+		{
+			payload: BuildPayload(&APS{
+				Alert: Alert{
+					Title: "Hello",
+				},
+			}, nil),
+			want: []byte(`{"aps":{"alert":{"title":"Hello"}}}`),
+		},
+	}
+
+	for _, tt := range tests {
+		n := &Notification{
+			Payload: tt.payload,
+		}
+
+		b, err := json.Marshal(n)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(b) != string(tt.want) {
+			t.Errorf("got: %v; want: %v", string(b), string(tt.want))
+		}
+	}
+}
+
+func TestNotificationPath(t *testing.T) {
+	n := &Notification{}
+	if n.Path() != "/3/device/" {
+		t.Errorf("got: %v; want: %v", n.Path(), "/3/device/")
+	}
+
+	n = &Notification{
+		DeviceToken: "7c968c83f6fd6de5843c309150ed1a706bc64fcdc42310f66054c0271e67219e",
+	}
+	if n.Path() != "/3/device/7c968c83f6fd6de5843c309150ed1a706bc64fcdc42310f66054c0271e67219e" {
+		t.Errorf("got: %v; want: %v", n.Path(), "/3/device/7c968c83f6fd6de5843c309150ed1a706bc64fcdc42310f66054c0271e67219e")
+	}
+}
+
+func TestNotificationURL(t *testing.T) {
+	n := &Notification{}
+	if n.URL() != "https://api.push.apple.com/3/device/" {
+		t.Errorf("got: %v; want: %v", n.URL(), "https://api.push.apple.com/3/device/")
+	}
+
+	n = &Notification{
+		Host: HostDevelopment,
+	}
+	if n.URL() != "https://api.sandbox.push.apple.com/3/device/" {
+		t.Errorf("got: %v; want: %v", n.URL(), "https://api.sandbox.push.apple.com/3/device/")
+	}
+
+	n = &Notification{
+		Host:        HostProductionPort2197,
+		DeviceToken: "7c968c83f6fd6de5843c309150ed1a706bc64fcdc42310f66054c0271e67219e",
+	}
+	if n.URL() != "https://api.push.apple.com:2197/3/device/7c968c83f6fd6de5843c309150ed1a706bc64fcdc42310f66054c0271e67219e" {
+		t.Errorf("got: %v; want: %v", n.URL(), "https://api.push.apple.com:2197/3/device/7c968c83f6fd6de5843c309150ed1a706bc64fcdc42310f66054c0271e67219e")
+	}
+}
+
+func TestNotificationSetHeaders(t *testing.T) {
+	n := &Notification{}
+	h := make(http.Header)
+	n.SetHeaders(h)
+	if len(h) != 0 {
+		t.Error("header must be empty")
+	}
+
+	n = &Notification{
+		ID:         "EC1BF194-B3B2-424A-89A9-5A918A6E6B5D",
+		Topic:      "com.example.app",
+		PushType:   PushTypeAlert,
+		Expiration: "1629000000",
+		Priority:   10,
+		CollapseID: "448C1AA7-B421-44D2-A995-2E4A7F1AE29E",
+	}
+	h = make(http.Header)
+	n.SetHeaders(h)
+
+	if h.Get("apns-id") != "EC1BF194-B3B2-424A-89A9-5A918A6E6B5D" {
+		t.Errorf("apns-id: %v; want: %v", h.Get("apns-id"), "EC1BF194-B3B2-424A-89A9-5A918A6E6B5D")
+	}
+	if h.Get("apns-topic") != "com.example.app" {
+		t.Errorf("apns-topic: %v; want: %v", h.Get("apns-topic"), "com.example.app")
+	}
+	if h.Get("apns-push-type") != "alert" {
+		t.Errorf("apns-push-type: %v; want: %v", h.Get("apns-push-type"), "alert")
+	}
+	if h.Get("apns-expiration") != "1629000000" {
+		t.Errorf("apns-expiration: %v; want: %v", h.Get("apns-expiration"), "1629000000")
+	}
+	if h.Get("apns-priority") != "10" {
+		t.Errorf("apns-priority: %v; want: %v", h.Get("apns-priority"), "10")
+	}
+	if h.Get("apns-collapse-id") != "448C1AA7-B421-44D2-A995-2E4A7F1AE29E" {
+		t.Errorf("apns-collapse-id: %v; want: %v", h.Get("apns-collapse-id"), "448C1AA7-B421-44D2-A995-2E4A7F1AE29E")
+	}
+}
+
+func TestNotificationBuildRequest(t *testing.T) {
+
+}
 
 func BenchmarkNotificationSimplePayload(b *testing.B) {
 	n := &Notification{
@@ -45,12 +173,14 @@ func BenchmarkNotificationFullPayload(b *testing.B) {
 				Name:     "Sound",
 				Volume:   0.1,
 			},
-			Category:         "Category",
-			ThreadID:         "ThreadID",
-			MutableContent:   1,
-			ContentAvailable: 1,
-			TargetContentID:  "TargetContentID",
-			URLArgs:          []string{"Arg1", "Arg2"},
+			Category:          "Category",
+			ThreadID:          "ThreadID",
+			MutableContent:    1,
+			ContentAvailable:  1,
+			TargetContentID:   "TargetContentID",
+			InterruptionLevel: InterruptionLevelActive,
+			RelevanceScore:    1.0,
+			URLArgs:           []string{"Arg1", "Arg2"},
 		}, nil),
 	}
 
